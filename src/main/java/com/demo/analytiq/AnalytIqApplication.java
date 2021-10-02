2b177675584497ba9bb4f5347dc3b2e89e4dfec2
Karththi <sharthi2017@gmail.com>
2021-10-02 19:51:26 +0530

    initial commit

---
+++ src/main/java/com/demo/analytiq/AnalytIqApplication.java
@@ -0,0 +1,13 @@
+package com.demo.analytiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalytIqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalytIqApplication.class, args);
+    }
+
+}
